f93820e3ed3e228a71a12c78703c45134ff06e86
toast1337 <58362380+t0ast1337@users.noreply.github.com>
2020-07-04 10:25:39 +0200

    Update Anny.py

--- Anny.py
+++ Anny.py
@@ -1,10 +1,12 @@
 # ONLY AUTHOR: t0ast1337 aka iTsToastZ
 # july 3rd 2020
 # youtube.com/itstoastz
+# NOTE: THERE IS A LITTLE BUG WHO ILL FIX LATER SO DONT DL IT RN
 
 import subprocess
 import colorama
 import requests
+import sys
 
 from colorama import *
 
@@ -21,6 +23,7 @@ def locate(ip):
 ''' + Fore.RED + Style.DIM + '''║ ''' + Fore.WHITE + Style.BRIGHT + '''Lon''' + Fore.BLACK + ''':''' + Fore.WHITE+ ''' ''' + json_data['lon'] + (' ' * (37 - int(len(json_data['lon'])))) + Fore.RED + Style.DIM + ''' ║
 ''' + Fore.RED + Style.DIM + '''╚════════════════════════════════════════════╝''')
 
+
 banner = f'''                                                                       
 {Fore.RED}{Style.DIM}.d8888b. 88d888b. 88d888b. dP    dP 
 {Fore.RED}{Style.DIM}88'  `88 88'  `88 88'  `88 88    88 
@@ -29,37 +32,38 @@ banner = f'''
 {Fore.BLACK}{Style.BRIGHT}oooooooooooooooooooooooooooo~~~~{Fore.RED}{Style.DIM}.88{Fore.BLACK}{Style.BRIGHT}~
                             {Fore.RED}{Style.DIM}d8888P{Style.BRIGHT}  
 {Fore.WHITE}AnyDesk {Fore.BLACK}{Style.BRIGHT}IP {Fore.WHITE}Resolver
-
 {Fore.RED}{Style.DIM}youtube.com/itstoastz
 github.com/t0ast1337
 telegram.me/haxx3r{Fore.WHITE}{Style.BRIGHT}
 '''
 
 print(banner)
-print("waiting for connection...")
+print("waiting for connection...", )
 while 1:
 	if str(subprocess.check_output("tasklist")).count('AnyDesk.exe') <= 3:
 		pass
+	else:
+		lines = str(subprocess.check_output("netstat -p TCP -n -o -a -b")).replace('b"', '"').replace('\\r', '').replace('\\n', '\n').split('\n')
+		n = 0
+		anydesk_lines = []
+		for line in lines:
+			if '[AnyDesk.exe]' in line:
+				anydesk_lines.append(lines[n - 1])
+			n += 1
 	
-	lines = str(subprocess.check_output("netstat -p TCP -n -o -a -b")).replace('b"', '"').replace('\\r', '').replace('\\n', '\n').split('\n')
-	n = 0
-	anydesk_lines = []
-	for line in lines:
-		if '[AnyDesk.exe]' in line:
-			anydesk_lines.append(lines[n - 1])
-		n += 1
+		ips = []
+	
+		for line in anydesk_lines:
+			if not '0.0.0.0' in line and 'ESTABLISHED' in line:
+				parts = line.split()
+				ips.append(parts[2])
+	
+		for _ip in ips:
+			try:
+				ip = _ip.split(':')[0]
+				if not '136.243' in str(ip): # anydesk web ip
+					locate(ip)
+					exit()
+			except Exception as e:
+				pass
 
-	ips = []
-
-	for line in anydesk_lines:
-		if not '0.0.0.0' in line and 'ESTABLISHED' in line:
-			parts = line.split()
-			ips.append(parts[2])
-
-	for _ip in ips:
-		try:
-			ip = _ip.split(':')[0]
-			if '136.243' in str(ip): # anydesk web ip
-			locate(ip)
-		except:
-			pass
