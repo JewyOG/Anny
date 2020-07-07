e884d68dd90b5df4bb200918f6207331f4abc9cb
toast1337 <58362380+t0ast1337@users.noreply.github.com>
2020-07-07 06:30:48 +0200

    Update Anny.py

--- Anny.py
+++ Anny.py
@@ -3,46 +3,59 @@
 # youtube.com/itstoastz
 # run as administrator
 # @sendnudespls on telegram for questions
+# yo photy :D
 
-import subprocess, colorama, requests, base64
+import subprocess, colorama, requests, base64, os
+
+os.system('@title Anny 4.0 ^| AnyDesk IP Address Resolver ^| by t0ast1337 && cls')
 
 from colorama import Fore, Style
 
 colorama.init()
 
+anydesk_pids = []
+anydesk_address = {}
+ip_addr = []
+old_port = 0
+old_ip = ''
+
 # banner
 print(base64.b64decode(b'G1sxbSAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgChtbMzFtLmQ4ODg4Yi4gODhkODg4Yi4gODhkODg4Yi4gZFAgICAgZFAgChtbMzFtODgnICBgODggODgnICBgODggODgnICBgODggODggICAgODggChtbMzFtODguICAuODggODggICAgODggODggICAgODggODguICAuODggChtbMzFtYDg4ODg4UDggZFAgICAgZFAgZFAgICAgZFAgYDg4ODhQODggCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgG1szMW0uODggChtbMzdtQW5ueSAbWzMwbS0bWzM3bSBBbnlEZXNrIElQIFJlc292bGVyICAgG1szMW1kODg4OFAgIAobWzM3bU1hZGUgYnkgZ2l0aHViLmNvbS90MGFzdDEzMzcK').decode().replace("haxx3r", "sendnudespls") + "\nwaiting for connection...", end='')
 
-# listener
 while 1:
 	try:
 		if str(subprocess.check_output("tasklist")).count('AnyDesk.exe') <= 3:
 			pass
 		else:
-			print(f' {Fore.GREEN}connection established! ')
-			lines = str(subprocess.check_output("netstat -p TCP -n -o -a -b")).replace('b"', '"').replace('\\r', '').replace('\\n', '\n').split('\n')
-			n = 0
-			anydesk_lines = []
-			ips = []
-			for line in lines:
-				if '[AnyDesk.exe]' in line:
-					anydesk_lines.append(lines[n - 1])
-				n += 1
-			for line in anydesk_lines:
-				if not '0.0.0.0' in line and 'ESTABLISHED' in line:
-					ips.append(line.split()[2])	
-			for ip in ips:
-				try:
-					# anydesk web ip
+			for line in str(subprocess.check_output("tasklist")).replace('b"', '"').replace('\\r', '').replace('\\n', '\n').split('\n'):
+				if 'AnyDesk.exe' in line:
 					try:
-						requests.get('https://'+str(ip.split(':')[0]), timeout=1)
+						anydesk_pids.append(line.split()[1])
 					except Exception as e:
-						if 'CERTIFICATE_VERIFY_FAILED' in str(e):
+						pass
+			nstats_output_lines = str(subprocess.check_output('netstat -p TCP -n -a -o')).replace('b"', '"').replace('\\r', '').replace('\\n', '\n').split('\n')
+			for pid in anydesk_pids:
+				for line in nstats_output_lines:
+					if pid in line and not 'LISTENING' in line:
+						try:
+							parts = line.split()
+							protocol = parts[0]
+							local_addr = parts[1]
+							remote_addr = parts[2].split(':')[0]
+							remote_port = parts[2].split(':')[1]
+							anydesk_address[remote_addr] = int(remote_port)
+						except Exception as e:
 							pass
-						else:
-							try:
-								json_data = requests.get(f'http://extreme-ip-lookup.com/json/' + ip.split(':')[0]).json()
-								print('''
+			for ip, port in anydesk_address.items():
+				if int(port) > old_port:
+					old_port = int(port)
+					old_ip = ip
+			remote_ip = old_ip
+			remote_port = old_port
+			print(f'{Fore.GREEN} connection established!')
+			try:
+				json_data = requests.get(f'http://extreme-ip-lookup.com/json/' + remote_ip).json()
+				print('''
 {12}╔════════════════════════════════════════════╗
 {12}║ {13}IP{14}:{13} {0}{6} {12}║ 
 {12}║ {13}Country{14}:{13} {1}{7} {12}║ 
@@ -51,13 +64,10 @@ while 1:
 {12}║ {13}Lat{14}:{13} {4}{10} {12}║ 
 {12}║ {13}Lon{14}:{13} {5}{11} {12}║ 
 {12}╚════════════════════════════════════════════╝
-		'''.format(json_data['query'], json_data['country'], json_data['city'], json_data['isp'], json_data['lat'], json_data['lon'], (' ' * (38 - int(len(json_data['query'])))), (' ' * (33 - int(len(json_data['country'])))), (' ' * (36 - int(len(json_data['city'])))), (' ' * (37 - int(len(json_data['isp'])))), (' ' * (37 - int(len(json_data['lat'])))), (' ' * (37 - int(len(json_data['lon'])))),Fore.RED, Fore.WHITE, Fore.BLACK))
-							except:
-								print('hum.')						
-				except Exception:
-					pass
+				'''.format(json_data['query'], json_data['country'], json_data['city'], json_data['isp'], json_data['lat'], json_data['lon'], (' ' * (38 - int(len(json_data['query'])))), (' ' * (33 - int(len(json_data['country'])))), (' ' * (36 - int(len(json_data['city'])))), (' ' * (37 - int(len(json_data['isp'])))), (' ' * (37 - int(len(json_data['lat'])))), (' ' * (37 - int(len(json_data['lon'])))),Fore.RED, Fore.WHITE, Fore.BLACK))
+			except:
+				print('hum.')	
 			exit()
 	except KeyboardInterrupt:
-		print('pressed ctrl+c! quitting...')
-		exit()
+		print('ctrl+c'); exit()
 
